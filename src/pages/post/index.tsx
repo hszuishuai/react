5f7a0d1023a1d8ebc17551695b410a5b949ae534
黄胜 <huangsheng@huangshengdeMacBook-Pro.local>
2020-03-23 10:09:35 +0800

    修改

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 const Post: React.SFC = () => {
-  const { id } = useParams();
+    const { id } = useParams();
 
-  return (
-    <div>
-      <span>2www</span>
-      {id}
-    </div>
-  );
+    return (
+        <div>
+            <span>2www</span>
+            {id}
+        </div>
+    );
 };
 
 export default Post;
